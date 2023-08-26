a769014f3c5da09d28df803e6c0eddd5e6c12156
gopi ananda naidu <gopianandakumar@gmail.com>
2023-08-26 13:05:15 +0530

    calculator using react js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Calculator from './Components/calculator';
+
+function App() {
+  return (
+    <div className="App">
+     <Calculator />
+    </div>
+  );
+}
+
+export default App;
